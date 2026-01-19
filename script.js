e6b9c28106b0f68aa8ab6c5d663a36fac24f39bb
dwp-loves-sso <rmirov@sacsheriff.com>
2026-01-19 13:35:12 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+const section1 = document.getElementById("section-1");
+const head = document.getElementById("head");
+const rightHand = document.getElementById("right-hand");
+const leftHand = document.getElementById("left-hand");
+const rightEye = document.getElementById("right-eye");
+const leftEye = document.getElementById("left-eye");
