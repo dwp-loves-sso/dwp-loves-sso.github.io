cea290fe39349eb9cfb29959cd97ae083d48cfd9
dwp-loves-sso <rmirov@sacsheriff.com>
2026-01-29 07:03:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let section1 = document.getElementById("section-1");
+let backBtn = document.getElementById("back-btn");
 let head = document.getElementById("head");
 let rightHand = document.getElementById("right-hand");
 let leftHand = document.getElementById("left-hand");
 let rightEye = document.getElementById("right-eye");
 let leftEye = document.getElementById("left-eye");
 
-let prevView = "show-body";
+let prevView = "show-person";
 
 function changeView(bodyPart) {
     section1.classList.remove(prevView);
@@ -18,3 +19,7 @@ rightHand.addEventListener("click", () => changeView("right-hand"));
 leftHand.addEventListener("click", () => changeView("left-hand"));
 rightEye.addEventListener("click", () => changeView("right-eye"));
 leftEye.addEventListener("click", () => changeView("left-eye"));
+
+function showPerson() {
+    changeView("person");
+}
