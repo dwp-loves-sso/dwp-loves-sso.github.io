22cbe29f355112997f1b710b6daa851f46dff3ec
dwp-loves-sso <rmirov@sacsheriff.com>
2026-01-29 07:04:27 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-let section1 = document.getElementById("section-1");
-let backBtn = document.getElementById("back-btn");
-let head = document.getElementById("head");
-let rightHand = document.getElementById("right-hand");
-let leftHand = document.getElementById("left-hand");
-let rightEye = document.getElementById("right-eye");
-let leftEye = document.getElementById("left-eye");
+let section1 = document.getElementById('section-1');
+let backBtn = document.getElementById('back-btn');
+let head = document.getElementById('head');
+let rightHand = document.getElementById('right-hand');
+let leftHand = document.getElementById('left-hand');
+let rightEye = document.getElementById('right-eye');
+let leftEye = document.getElementById('left-eye');
 
-let prevView = "show-person";
+let prevView = 'show-person';
 
 function changeView(bodyPart) {
     section1.classList.remove(prevView);
-    section1.classList.add("show-" + bodyPart);
-    prevView = "show-" + bodyPart;
+    section1.classList.add('show-' + bodyPart);
+    prevView = 'show-' + bodyPart;
 }
 
-head.addEventListener("click", () => changeView("head"));
-rightHand.addEventListener("click", () => changeView("right-hand"));
-leftHand.addEventListener("click", () => changeView("left-hand"));
-rightEye.addEventListener("click", () => changeView("right-eye"));
-leftEye.addEventListener("click", () => changeView("left-eye"));
+head.addEventListener('click', () => changeView('head'));
+rightHand.addEventListener('click', () => changeView('right-hand'));
+leftHand.addEventListener('click', () => changeView('left-hand'));
+rightEye.addEventListener('click', () => changeView('right-eye'));
+leftEye.addEventListener('click', () => changeView('left-eye'));
 
 function showPerson() {
-    changeView("person");
+    changeView('person');
+    backBtn.style.display = 'none';
 }
